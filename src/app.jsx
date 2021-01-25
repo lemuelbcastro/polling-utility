3b3bcd587ef6e075b61b5955ab3d7424b6e3c1de
Lemuel Castro <lemuelbercasiocastro@gmail.com>
2021-01-25 09:58:59 +0800

    Add react and material-ui

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import "fontsource-roboto";
+
+ReactDOM.render(
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>,
+  document.getElementById("root")
+);
--- webpack.rules.js
+++ webpack.rules.js
@@ -2,17 +2,31 @@ module.exports = [
   // Add support for native node modules
   {
     test: /\.node$/,
-    use: 'node-loader',
+    use: "node-loader",
   },
   {
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      loader: "@marshallofsound/webpack-asset-relocator-loader",
       options: {
-        outputAssetBase: 'native_modules',
+        outputAssetBase: "native_modules",
       },
     },
+  },
+  {
+    test: /\.jsx?$/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        exclude: /node_modules/,
+        presets: ["@babel/preset-react"],
+      },
+    },
+  },
+  {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    use: [{ loader: "file-loader" }],
   },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
